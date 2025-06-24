0d0ee49817973f7db23cb4e8f9e2be041374b511
Anna Denisyak <anndenisyak2580@gmail.com>
2025-06-24 19:48:30 +0300

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html' 
+            template: './index.html' 
           }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
